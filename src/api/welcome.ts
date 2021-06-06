e2b0e4d991fa669a7993b19c581833e0ed863715
Jose Silva <me@jsi1v4.dev>
2021-06-06 11:50:47 -0300

    feat: finish welcome route

--- src/api/welcome.ts
+++ src/api/welcome.ts
@@ -1,5 +1,32 @@
+import fetch from "node-fetch";
+
 import { Request, Response } from "lib/handler";
 
-export default function handler(req: Request, res: Response) {
-  res.status(200).json({ name: `I'm Jose Silva...` });
+function breakLines(text: string) {
+  return text.split("\r\n").slice(0, -1);
 }
+
+async function getResume() {
+  return fetch(
+    "https://raw.githubusercontent.com/jsi1v4/jsi1v4/main/topics/resume.md"
+  )
+    .then((res) => res.text())
+    .then(breakLines);
+}
+
+async function getLinks() {
+  return fetch(
+    "https://raw.githubusercontent.com/jsi1v4/jsi1v4/main/topics/links.md"
+  )
+    .then((res) => res.text())
+    .then(breakLines);
+}
+
+async function handler(req: Request, res: Response) {
+  const resume = await getResume();
+  const links = await getLinks();
+
+  res.status(200).json({ name: `I'm Jose Silva...`, resume, links });
+}
+
+export default handler;
